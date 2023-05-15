6aea9693573a7c0f5c6b25a7a8c5c42f82fda490
Nick JDA <armstrongnick1999@gmail.com>
2023-05-15 06:13:26 -0400

    Addedplain english pseudo code to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@ var APIKey = "2cf8696659dd95e4c36499af01e37d9c";
 var city = "";
 var state = "";
 var country = "";
+
+//take searched for city name and save it to local storage
+    //display city name and date on results section
+    //return relevent weather data
+        //current temp, wind, and humidity of city data
+            //5-day forecast
+                //each day has date, temp, wind, and humidity data
+    //upon search, button with city name is saved below search bar
+        //when button is clicked, return that cities weather results
