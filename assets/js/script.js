6117b4c516ea7d2b1e33ba953da9203b1bb300a1
Nick JDA <armstrongnick1999@gmail.com>
2023-05-15 06:52:47 -0400

    Added start of searchAPI function to js, added start of local storage saving function to js, moved button element in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var APIKey = "2cf8696659dd95e4c36499af01e37d9c";
 var city = "";
 var state = "";
 var country = "";
+var cityForecastContent = $("city-forecast-content");
 
 //take searched for city name and save it to local storage
     //display city name and date on results section
@@ -11,3 +12,20 @@ var country = "";
                 //each day has date, temp, wind, and humidity data
     //upon search, button with city name is saved below search bar
         //when button is clicked, return that cities weather results
+
+// function searchAPI() {
+
+// }
+
+//saving and displaying local storage jquery
+// $('.searchBtn').on('click', function() {
+//     console.log($(this).siblings('#search-input').val())
+//     var key = $(this).siblings('#past-search')
+//     var value = $(this).siblings('#search-input').val()
+//     localStorage.setItem(key, value);
+//   })
+
+// $('.searchBtn').each(function() {
+//     var searchHist = $(this).attr('id');
+//     $(this).siblings('button').val(localStorage.getItem(searchHist));
+//   })
