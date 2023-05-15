17f67b3ee26204fbf12f9106c7e28a76be54ecbd
Nick JDA <armstrongnick1999@gmail.com>
2023-05-15 18:27:10 -0400

    Added search api functionality, fixed search history display function and added search capability to past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var APIKey = "2cf8696659dd95e4c36499af01e37d9c";
+var apiKey = "2cf8696659dd95e4c36499af01e37d9c";
 var city = "";
 var state = "";
 var country = "";
 var cityForecastContent = $("city-forecast-content");
+// savedCities will either be the localstorage under the key "savedCities", if there is no localStorage, then it will be an empty array
+var savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
+// JSON.parse can used for objects OR arrays
 
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //take searched for city name and save it to local storage
     //display city name and date on results section
@@ -16,22 +18,89 @@ var cityForecastContent = $("city-forecast-content");
     //upon search, button with city name is saved below search bar
         //when button is clicked, return that cities weather results
 
-// function searchAPI() {
+function searchAPI(query) {
+    var fetchURL = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`;
 
-// }
+    fetch(fetchURL)
+        .then(function (response) {
+            if (!response.ok) {
+              throw response.json();
+            }
+            return response.json();
+})
+.then(function(results) {
+    console.log(results);
+    // Capture lat and lon and save it to a variable, build a second function to fetch the actual weather data
+    secondSearchAPI(results[0].lat, results[0].lon);
+})
+.catch(function (error) {
+    console.error(error);
+})
+};
+
+function secondSearchAPI(lat, lon) {
+    var fetchURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+    fetch(fetchURL)
+    .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+        return response.json();
+})
+.then(function(results) {
+console.log(results);
+// Create and append the daily forecast and the five day forecast
+})
+.catch(function (error) {
+console.error(error);
+})
+}
+
+function displayPastSearches() {
+    $('#past-searches').empty();
+    savedCities.forEach(city => {
+        $('#past-searches').append($('<button></button>').text(city).addClass('btn btn-info btn-block w-100 pastSearch'));
+    })
+};
 
 //saving and displaying local storage jquery
-$('.searchBtn').on('click', function() {
-    console.log($(this).siblings('#search-input').val())
-    var key = $(this).siblings('#past-search')
+$('.searchBtn').on('click', function(event) {
+    event.preventDefault();
+    // console.log($(this).siblings('#search-input').val())
+    // var key = $(this).siblings('#past-search')
     var value = $(this).siblings('#search-input').val()
-    localStorage.setItem(key, value);
+    if(value !== "") {
+        savedCities = savedCities.filter(city => city != value);
+        savedCities.push(value);
+        localStorage.setItem('savedCities', JSON.stringify(savedCities));
+    }
+    displayPastSearches();
+    searchAPI(value);
+  });
+
+// $('.pastSearch').each(function(button) {
+//     button.on('click', function(event) {
+//     // $(this).on('click', function(event) {
+//         event.preventDefault();
+//         var value = $(this).text();
+//         searchAPI(value);
+//     })
+//   })
+
+  $('#past-searches').on('click', function(event) {
+    // Added eventlistner to the div
+    console.log(event);
+    // If the target tag that was clicked on is defined as a button, then it will run our api fetch
+    if(event.target.localName === 'button') {
+        // instead of capturing the input value, we capture the button text
+        var value = event.target.textContent;
+        searchAPI(value);
+    }
   })
 
-$('.searchBtn').each(function() {
-    var searchHist = $(this).attr('id');
-    $(this).siblings('button').val(localStorage.getItem(searchHist));
-  })
+
+displayPastSearches();
 
 
 
